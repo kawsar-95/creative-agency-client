234e6b42f4dbac45ae1dda71b7ef917b8c6ad840
kawsar-95 <virtualkawsar@gmail.com>
2020-10-17 19:27:17 +0600

    first commit

---
+++ src/component/Login/firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCdzomZYKUUeAyvzpWDvNo-csvAoHMGC1c",
+  authDomain: "creative-agenc.firebaseapp.com",
+  databaseURL: "https://creative-agenc.firebaseio.com",
+  projectId: "creative-agenc",
+  storageBucket: "creative-agenc.appspot.com",
+  messagingSenderId: "254766838263",
+  appId: "1:254766838263:web:c682e5b29399e5227e7ef2"
+};
+  export default firebaseConfig;
